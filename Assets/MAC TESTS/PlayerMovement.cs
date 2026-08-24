ff77b0fa96efd5643d9870b85f2b407281944f40
Ruminouz <macirallam@gmail.com>
2026-08-24 12:41:38 +0800

    Character 8Directional Animation
    N NW NE
    S SW SE
    W E

---
+++ Assets/MAC TESTS/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+  private void FixedUpdate()
+{
+    rb.linearVelocity = moveInput * moveSpeed;
+
+    Debug.Log($"Position: {rb.position} | Velocity: {rb.linearVelocity}");
+}
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        // Read raw input
+        Vector2 input = context.ReadValue<Vector2>();
+
+        if (context.performed)
+        {
+            moveInput = input;
+            animator.SetBool("isWalking", true);
+
+            // Update direction vectors while moving
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+        }
+        else if (context.canceled)
+        {
+            // Store last valid movement direction BEFORE clearing moveInput
+            if (moveInput != Vector2.zero)
+            {
+                animator.SetFloat("LastInputX", moveInput.x);
+                animator.SetFloat("LastInputY", moveInput.y);
+            }
+
+            moveInput = Vector2.zero;
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
