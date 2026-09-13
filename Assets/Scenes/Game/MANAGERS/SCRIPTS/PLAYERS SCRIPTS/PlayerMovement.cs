61c4efa75aa9cdf3945ff6ac264d251c42802b29
jameschuayyy <165542319+jameschuayyy@users.noreply.github.com>
2026-09-14 02:35:17 +0800

    Revert "test"
    This reverts commit 5f2e2ac1d4a5c329f2d8aa9af767404ddd098407.

--- Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
+++ Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
@@ -18,18 +18,6 @@ public class PlayerMovement : MonoBehaviour
 
   private void FixedUpdate()
 {
-<<<<<<< Updated upstream:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
-=======
-<<<<<<< Updated upstream:Assets/MAC TESTS/PlayerMovement.cs
-<<<<<<< Updated upstream:Assets/MAC TESTS/PlayerMovement.cs
-    rb.linearVelocity = moveInput * moveSpeed;
-
-   
-=======
-=======
-<<<<<<< Updated upstream:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
->>>>>>> Stashed changes:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
->>>>>>> Stashed changes:Assets/MAC TESTS/PlayerMovement.cs
     if(PauseController.IsGamePaused)
         {
             rb.linearVelocity = Vector2.zero;
@@ -38,18 +26,6 @@ public class PlayerMovement : MonoBehaviour
         }
     rb.linearVelocity = moveInput * moveSpeed;
     animator.SetBool("isWalking", rb.linearVelocity.magnitude > 0);
-<<<<<<< Updated upstream:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
-=======
-<<<<<<< Updated upstream:Assets/MAC TESTS/PlayerMovement.cs
->>>>>>> Stashed changes:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
-=======
-=======
-    rb.linearVelocity = moveInput * moveSpeed;
-
-   
->>>>>>> Stashed changes:Assets/MAC TESTS/PlayerMovement.cs
->>>>>>> Stashed changes:Assets/Scenes/Game/MANAGERS/SCRIPTS/PLAYERS SCRIPTS/PlayerMovement.cs
->>>>>>> Stashed changes:Assets/MAC TESTS/PlayerMovement.cs
 }
 
     public void Move(InputAction.CallbackContext context)
